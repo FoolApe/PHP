5b30ad3f67179686814e8963d03759ade7cd7d31
FoolApe <a22951701a@gmail.com>
2023-02-22 16:30:39 +0800

    Add esxi_nic.php and update esxi_info.php

--- esxi_info.php
+++ esxi_info.php
@@ -1,92 +1,83 @@
 <?php
 
 	// VARs
-	$token = isset($_GET['token']) ? $_GET['token'] : '';
-    $host = isset($_GET['host']) ? $_GET['host'] : '';
-	$type = isset($_GET['type']) ? $_GET['type'] : '';
+	$token = $_GET['token']??'';
+    $host = $_GET['host']??'';
+	$type = $_GET['type']??'';
 	$port = '22';
-	$user = 'root';
-	$pass = 'ESXI_PASSWD';
-	$token_num = 'USER_DEFINE_TOKEN'; ### 防止陌生人亂call
+	$user = 'USER';
+	$pass = 'PASSWD';
+	$token_num = 'CUSTOM_TOKEN_420';
 
-	if ( $token == $token_num )
+	if ( $token !== $token_num )
 	{
-		if (!$host or !$type)
-		{
-			echo "Host = ESXI_IP.\ntype =  SN / vlan / Mode / Rack / SNMP / IPMI / Vendor / Model / Uptime / Hostname.\n";
-		}
+         echo "Wrong Token.\n";
+         exit;
+    }
+	if (!$host || !$type)
+	{
+		echo "Host = ESXI_IP.\ntype =  SN / vlan / Mode / Rack / SNMP / IPMI / Vendor / Model / Uptime / Hostname.\n";
+        exit;
+	}
 		
-		else
-		{
-			// SSH process
-			$conn = ssh2_connect($host, $port);
-			ssh2_auth_password($conn, 'root', $pass);
+	// SSH process
+	$ssh2_conn = ssh2_connect($host, $port, [], ['connect_timeout' => 10]);
+	ssh2_auth_password($ssh2_conn, $user, $pass);
 	
-			// Type options
-			switch($type)
-			{
-				case "SN":
-				$command = "esxcli hardware platform get |grep 'Serial Number' |grep -v 'Enclosure' |awk -F \: '{print $2}' |sed 's/[[:space:]]//g'";
-				break;
-
-				case "vlan":
-				$command = "esxcli network vswitch standard portgroup list |awk -F'vSwitch' '{print $2}' |awk '{print $3}' |sort -n |uniq |sed '/^$/d'";
-				break;
-
-				case "Mode":
-                $command = "esxcli system maintenanceMode get";
-                break;
-
-				case "Rack":
-				#$command = "vim-cmd hostsvc/net/query_networkhint --pnic-name=vmnic5 |grep 'systemName =' |awk -F \= '{print $2}' |awk -F \. '{print $1}' |sed 's/\"//g' |sed 's/r/R/g' |sed 's/[[:space:]]//g'";
-                $command = "vim-cmd hostsvc/net/query_networkhint --pnic-name=vmnic5 |grep 'devId =' |awk -F \= '{print $2}' |awk -F \. '{print $1}' |sed 's/\"//g' |sed 's/[[:space:]]//g' |sed 's/r/R/g'";
-				break;
-
-				case "SNMP":
-                $command = "esxcli system snmp get |egrep 'Communities|Enable' |awk -F \: '{print $2}'";
-                break;
-
-				case "IPMI":
-            	shell_exec("sshpass -p $pass scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /usr/local/ESXI-update/ipmitool root@'$host':/tmp "); // 傳送ipmitool
-				$command = "/tmp/ipmitool lan print |grep 'IP Address' |grep -v 'Source' |awk -F \: '{print $2}' |sed 's/[[:space:]]//g'";
-                break;
-
-				case "Vendor";
-				$command = "esxcli hardware platform get |grep Vendor |sed 's/Vendor//g' |sed 's/Name//g' |sed 's/\://g' |sed 's/^     //g' |sed 's/Inc\.//g'";
-				break;
-
-				case "Model";
-                $command = "esxcli hardware platform get |grep 'Product Name' |awk -F \: '{print $2}' |sed s'/PowerEdge//'g |sed s'/ProLiant//'g |sed s'/DL360//'g |sed s'/System//'g |sed s'/x3550//'g |sed s'/\://'g |sed s'/Think//'g |sed s'/IBM//'g |sed 's/\-\[7X02CTO1WW\]\-//g' |sed 's/\-\[5463IFE\]\-//g' |sed 's/[[:space:]]//g'";
-                break;
-
-				case "Uptime";
-          		$command = "/bin/uptime |awk -F \, '{print $1}' |awk -F'up\' '{print $2}'";     
-				break;
-
-				case "Hostname";
-                $command = "esxcli system hostname get |grep 'Host Name' |awk -F \: '{print $2}' |sed 's/^\ //'";
-                break;
-
-				case "BIOS";
-                $command = "smbiosDump  |grep -A3 'BIOS Info' |grep Version |awk -F\: '{print $2}' |sed 's/[[:space:]]//g'";
-                break;
-
-				default:
-				echo "Host = ESXI_IP.\ntype =  SN / vlan / Mode / Rack / SNMP / IPMI / Vendor / Model / Uptime / Hostname.\n";
-			}
-			
-			// stream settings
-			$stream = ssh2_exec($conn, "$command");
-			stream_set_blocking($stream, true);
-			$stream_out=ssh2_fetch_stream($stream, SSH2_STREAM_STDIO); //大小寫有差
-	
-			// Output
-			echo trim(stream_get_contents($stream_out));
-		}
-	}
-
-	else
+	// Type options
+	switch($type)
 	{
-		echo "Wrong Token.\n";
+		case "SN":
+		$command = "esxcli hardware platform get |grep 'Serial Number' |grep -v 'Enclosure' |awk -F \: '{print $2}' |sed 's/[[:space:]]//g'";
+		break;
+
+		case "vlan":
+		$command = "esxcli network vswitch standard portgroup list |awk -F'vSwitch' '{print $2}' |awk '{print $3}' |sort -n |uniq |sed '/^$/d'";
+		break;
+
+		case "Mode":
+        $command = "esxcli system maintenanceMode get";
+        break;
+
+		case "Rack":
+        $command = "vim-cmd hostsvc/net/query_networkhint --pnic-name=vmnic6 |grep 'devId =' |awk -F \= '{print $2}' |awk -F \. '{print $1}' |sed 's/\"//g' |awk -F \- '{print $1}' |sed 's/[[:space:]]//g'";
+		break;
+
+		case "SNMP":
+        $command = "esxcli system snmp get |egrep 'Communities|Enable' |awk -F \: '{print $2}'";
+        break;
+
+		case "IPMI":
+    	shell_exec("sshpass -p $pass scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /var/www/html/package/ipmitool root@'$host':/tmp "); // 傳送ipmitool
+		$command = "/tmp/ipmitool lan print |grep 'IP Address' |grep -v 'Source' |awk -F \: '{print $2}' |sed 's/[[:space:]]//g'";
+        break;
+
+		case "Vendor";
+		$command = "esxcli hardware platform get |grep Vendor |sed 's/Vendor//g' |sed 's/Name//g' |sed 's/\://g' |sed 's/^     //g' |sed 's/Inc\.//g'";
+		break;
+
+		case "Model";
+        $command = "esxcli hardware platform get |grep 'Product Name' |awk -F \: '{print $2}' |sed 's/^\ //g'";
+        break;
+
+		case "Uptime";
+    	$command = "/bin/uptime |awk -F \, '{print $1}' |awk -F'up\' '{print $2}' |sed 's/^\ //g'";     
+		break;
+
+		case "Hostname";
+        $command = "esxcli system hostname get |grep 'Host Name' |awk -F \: '{print $2}' |sed 's/^\ //'";
+        break;
+
+		default:
+		echo "Host = ESXI_IP.\ntype =  SN / vlan / Mode / Rack / SNMP / IPMI / Vendor / Model / Uptime / Hostname.\n";
 	}
+	
+	// stream settings
+	$stream = ssh2_exec($ssh2_conn, "$command");
+	stream_set_blocking($stream, true);
+    stream_set_timeout($stream, 1);
+	$stream_out=ssh2_fetch_stream($stream, SSH2_STREAM_STDIO); //大小寫有差
+	
+	// Output
+	echo trim(stream_get_contents($stream_out));
 ?>
---
+++ esxi_nic.php
@@ -0,0 +1,65 @@
+<?php
+
+	// VARs
+	$token = $_GET['token']??''; // php7 以後的語法 , 縮減長判斷式
+    $host = $_GET['host']??'';
+	$type = $_GET['type']??'';
+    $vmnic = $_GET['vmnic']??'';
+	$port = '22';
+	$user = 'USER';
+	$pass = 'PASSWD';
+	$token_num = 'CUSTOM_TOKEN_420'; ### jackey666 < MD5加密
+
+	if ( $token !== $token_num )
+	{
+        echo "Wrong Token.\n";
+        exit;
+    }
+
+    if (!$host || !$type) 
+    {
+        echo "Host = ESXI_IP.\ntype =  SN / vlan / Mode / Rack / SNMP / IPMI / Vendor / Model / Uptime / Hostname.\n";
+        exit;
+    }
+
+	// SSH process
+	$ssh2_conn = ssh2_connect($host, $port, [], ['connection_timeout => 3']);
+    if (!$ssh2_conn)
+    {
+        throw new Exception("Unable to establish SSH connection");
+    }
+	ssh2_auth_password($ssh2_conn, $user, $pass);
+    
+	// Type options
+	switch($type)
+	{
+        case "port"; // 對接vswitch_port
+        $command = "vim-cmd hostsvc/net/query_networkhint --pnic-name='$vmnic' |grep 'portId' |awk -F \= '{print $2}' |sed 's/\,//g' |sed 's/\"//g' |sed 's/[[:space:]]//g'";
+        break;
+
+        case "switch"; // 對接實體switch名稱(櫃位)
+        $command = "vim-cmd hostsvc/net/query_networkhint --pnic-name='$vmnic' |grep 'devId' |awk -F \= '{print $2}' |sed 's/\,//g' |sed 's/\"//g' |sed 's/[[:space:]]//g'";
+        break;
+
+        case "switch_vlan"; // 對接實體switch開放的vlan
+        $command = "vim-cmd hostsvc/net/query_networkhint --pnic-name='$vmnic' |grep vlanId |awk -F \= '{print $2}' |sort -n |xargs |sed 's/[[:space:]]//g' |sed 's/,$//'";
+        break;
+
+        case "speed"; 
+        $command = "esxcli network nic list |grep '$vmnic' |awk '{print $6}'";
+        break;
+
+		default:
+		echo "Host = ESXI_IP.\ntype =  SN / vlan / Mode / Rack / SNMP / IPMI / Vendor / Model / Uptime / Hostname.\n";
+        exit;
+	}
+	
+	// stream settings
+	$stream = ssh2_exec($ssh2_conn, "$command");
+	stream_set_blocking($stream, true);
+    stream_set_timeout($stream, 1);
+	$stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO); //大小寫有差
+	
+	// Output
+	echo trim(stream_get_contents($stream_out));
+?>
